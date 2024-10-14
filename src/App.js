cc2f05fb33ced64c3b05ab2c530637b60e59f01f
jeremy collyer <collyerdesign@gmail.com>
2024-10-14 15:10:39 -0700

    clean url input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>URL Scrape & Auto-fill</h1>
+  
+        
+        <form>
+          <div>
+            <h3>Enter URL to begin</h3>
+            <input type="text" />
+          </div>
+
+          <div>
+          <h3>Enter Prompt</h3>
+            <input type="text" />
+          </div>
+
+          <button>Submit</button>
+        </form>
     </div>
   );
 }
